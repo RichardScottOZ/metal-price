5093621d381e8ea72bc47ceb0f7904ab677aa257
Tommy Chu <tommychu2256@gmail.com>
2020-07-09 21:03:01 +0200

    moved to api-server

--- server/app/handlers/response_model.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-// response is a model for the response.
-type response struct {
-	Metal    string  `json:"metal"`
-	Price    float64 `json:"price"`
-	Currency string  `json:"currency"`
-	Unit     string  `json:"unit"`
-}
