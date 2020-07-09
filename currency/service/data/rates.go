33f706f9c56b06a6c81a27e6aa1d150efd962db6
Tommy Chu <tommychu2256@gmail.com>
2020-07-09 17:01:46 +0200

    wrap into service package

---
+++ currency/service/data/rates.go
@@ -0,0 +1,109 @@
+package data
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/thedevsaddam/gojsonq"
+)
+
+// Rates defines the exchange rates.
+type Rates struct {
+	log    *log.Logger
+	rates  map[string]float64
+	source string
+}
+
+// NewRates returns an new empty data service.
+func NewRates(l *log.Logger, src string) (*Rates, error) {
+
+	// contruct
+	r := &Rates{
+		log:    l,
+		rates:  map[string]float64{},
+		source: src,
+	}
+
+	// update rates
+	err := r.getRates()
+	if err != nil {
+		return nil, fmt.Errorf("could not update exchange rates: %w", err)
+	}
+
+	return r, nil
+}
+
+// GetRate returns rate of the two currencies.
+func (r *Rates) GetRate(base, dest string) (float64, error) {
+
+	// update
+	err := r.getRates()
+	if err != nil {
+		r.log.Printf("Could not update prices, using the data from the last call.")
+	}
+
+	// validation
+	d, ok := r.rates[dest]
+	if !ok {
+		return 0, fmt.Errorf("rate not found for currency: %s", dest)
+	}
+	b, ok := r.rates[base]
+	if !ok {
+		return 0, fmt.Errorf("rate not found for currency: %s", base)
+	}
+
+	// calculate
+	rate := d / b
+	return rate, nil
+}
+
+// getRates updates exchange rates for the data service Rates.
+func (r *Rates) getRates() error {
+
+	body, err := currencyAPI(r.source)
+	if err != nil {
+		return fmt.Errorf("currency api error: %w", err)
+	}
+
+	// query base and rates
+	base := gojsonq.New().FromString(string(body)).Find("base").(string)
+	rates := gojsonq.New().FromString(string(body)).From("rates").Get().(map[string]interface{})
+
+	// set rates
+	r.rates[base] = 1.0
+	for c, rat := range rates {
+		r.rates[c] = rat.(float64)
+	}
+
+	// success
+	return nil
+}
+
+// currencyAPI returns the response body and the error
+// from requesting an currency API.
+func currencyAPI(api string) ([]byte, error) {
+
+	// requesting
+	resp, err := http.Get(api)
+	if err != nil {
+		return nil, fmt.Errorf("could not request the currency api: %w", err)
+	}
+
+	// check status code
+	if resp.StatusCode != 200 {
+		return nil, errors.New("expected status code 200")
+	}
+
+	// read response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read currency api response body: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// success
+	return body, nil
+}
---
+++ currency/service/service.go
@@ -0,0 +1,64 @@
+package service
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/chutified/metal-price/currency/config"
+	"github.com/chutified/metal-price/currency/service/data"
+	"github.com/chutified/metal-price/currency/service/protos/currency"
+	"github.com/chutified/metal-price/currency/service/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+// Service is the service controller.
+type Service struct {
+	logger *log.Logger
+	srv    *grpc.Server
+}
+
+// NewService constructs a new service controller.
+func NewService(l *log.Logger) *Service {
+	return &Service{
+		logger: l,
+	}
+}
+
+// Init defines service attributes.
+func (s *Service) Init(cfg *config.Config) error {
+
+	// data service
+	rateService, err := data.NewRates(s.logger, cfg.Source)
+	if err != nil {
+		s.logger.Fatalf("could not construct currency price data service: %v", err)
+	}
+
+	// servers
+	currencyServer := server.NewCurrency(s.logger, rateService)
+	grpcSrv := grpc.NewServer()
+
+	// register the server
+	currency.RegisterCurrencyServer(grpcSrv, currencyServer)
+	// reflection responses
+	reflection.Register(grpcSrv)
+
+	// success
+	s.srv = grpcSrv
+	return nil
+}
+
+// Run starts the service.
+func (s *Service) Run(cfg *config.Config) error {
+
+	// define listen
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		return fmt.Errorf("unable to listen: %w", err)
+	}
+
+	// listen
+	s.logger.Printf("Listening gRPC on port %d", cfg.Port)
+	return s.srv.Serve(listen)
+}
