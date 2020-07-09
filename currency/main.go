81f40f41354f05911613f4ed691ed931f6092896
Tommy Chu <tommychu2256@gmail.com>
2020-07-09 17:02:38 +0200

    simplify, move into service mod

--- currency/main.go
+++ currency/main.go
@@ -1,46 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 	"os"
 
 	"github.com/chutified/metal-price/currency/config"
-	"github.com/chutified/metal-price/currency/data"
-	"github.com/chutified/metal-price/currency/protos/currency"
-	"github.com/chutified/metal-price/currency/server"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/chutified/metal-price/currency/service"
 )
 
 func main() {
-	logger := log.New(os.Stdout, "currency-service: ", log.LstdFlags)
+	logger := log.New(os.Stdout, "[CURRENCY SERVICE] ", log.LstdFlags)
 
+	// config
 	cfg, err := config.GetConfig("config.yaml")
 	if err != nil {
 		logger.Fatalf("get config: %v", err)
 	}
 
-	// data service
-	rateService, err := data.NewRates(logger, cfg.Source)
+	// init service
+	s := service.NewService(logger)
+	err = s.Init(cfg)
 	if err != nil {
-		logger.Fatalf("unable to construct data service: %v", err)
+		logger.Fatalf("initialize the service: %v", err)
 	}
 
-	// server
-	currencyServer := server.NewCurrency(logger, rateService)
-	grpcSrv := grpc.NewServer()
-
-	// register the server
-	currency.RegisterCurrencyServer(grpcSrv, currencyServer)
-	// reflection responses
-	reflection.Register(grpcSrv)
-
-	// run server
-	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
-	if err != nil {
-		logger.Fatalf("unable to listen: %v", err)
-	}
-	grpcSrv.Serve(listen)
+	// run
+	logger.Fatalf("run the service: %v", s.Run(cfg))
 }
