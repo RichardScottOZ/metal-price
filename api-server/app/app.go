b67d712d1071e3e32d693f2486fb1cf29657b81e
Tommy Chu <tommychu2256@gmail.com>
2020-07-09 21:02:25 +0200

    change folder name

---
+++ api-server/app/app.go
@@ -0,0 +1,89 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/chutified/metal-price/api-server/app/handlers"
+	"github.com/chutified/metal-price/api-server/app/services"
+	"github.com/chutified/metal-price/api-server/config"
+	"github.com/chutified/metal-price/currency/service/protos/currency"
+	"github.com/chutified/metal-price/metal/service/protos/metal"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+// App is a server controller.
+type App struct {
+	logger      *log.Logger
+	server      *http.Server
+	engine      *gin.Engine
+	connections []*grpc.ClientConn
+}
+
+// NewApp returns new App controller.
+func NewApp(l *log.Logger) *App {
+	return &App{
+		logger: l,
+	}
+}
+
+// Init sets everything for the App controller.
+func (a *App) Init(cfg *config.Config) error {
+
+	// debug mode
+	if !(cfg.Debug) {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	// currency client
+	currencyConn, err := grpc.Dial(cfg.CurrencyService, grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("unable to dial: %w", err)
+	}
+	a.connections = append(a.connections, currencyConn) // CONN
+	cs := services.NewCurrency(currency.NewCurrencyClient(currencyConn))
+
+	// metal client
+	metalConn, err := grpc.Dial(cfg.MetalService, grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("unable to dial: %w", err)
+	}
+	a.connections = append(a.connections, metalConn) // CONN
+	ms := services.NewMetal(metal.NewMetalClient(metalConn))
+
+	// construct an engine
+	handler := handlers.NewHandler(a.logger, cs, ms)
+	a.SetRoutes(handler) // ENGINE
+
+	// server
+	a.server = &http.Server{ // SERVER
+		Addr:              fmt.Sprintf(":%d", cfg.Port),
+		Handler:           a.engine,
+		ReadTimeout:       4 * time.Second,
+		ReadHeaderTimeout: 2 * time.Second,
+		WriteTimeout:      4 * time.Second,
+		IdleTimeout:       6 * time.Second,
+		MaxHeaderBytes:    http.DefaultMaxHeaderBytes,
+	}
+
+	// success
+	return nil
+}
+
+// Stop gracefully close all service's connections.
+func (a *App) Stop() []error {
+	var errs []error
+	for _, conn := range a.connections {
+		errs = append(errs, conn.Close())
+	}
+	return errs
+}
+
+// Run starts the server.
+func (a *App) Run() error {
+	a.logger.Printf("Listening and serving HTTP on port %s.\n", a.server.Addr)
+	return a.server.ListenAndServe()
+}
---
+++ api-server/app/services/periodic-symbols.go
@@ -0,0 +1,11 @@
+package services
+
+// PeriodicSymbols is a map of the symbols of the available metals in the periodic table.
+var PeriodicSymbols = map[string]string{
+	"au": "gold",
+	"ag": "silver",
+	"pt": "platinum",
+	"pd": "palladium",
+	"cu": "copper",
+	"rh": "rhodium",
+}
